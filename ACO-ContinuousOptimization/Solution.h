7711902f0fc48e8685b7388f8bad24a04564f036
komolebo <komolebo@gmail.com>
2016-10-12 22:59:07 +0300

    Update Solution.h

--- ACO-ContinuousOptimization/Solution.h
+++ ACO-ContinuousOptimization/Solution.h
@@ -3,7 +3,7 @@
 #include "Functions.h"
 #include <iostream>
 #include <vector>
-#include <cmath>			// For constants
+#include <cmath>		// For constants
 
 using namespace std;
 
@@ -15,11 +15,11 @@ class Solution
 
 public:
 	Solution(double x_min, double x_max, int dim, func f);	// First assignment
-	Solution(const vector<double> _S, func f);				// New pheromone
+	Solution(const vector<double> _S, func f);		// New pheromone
 	~Solution() {};
 
 	double get_value(int index) const { return S[index]; };		// x[i], where i - dimension
-	double get_f() const { return F; }							// function value on this points
+	double get_f() const { return F; }				// function value on this points
 	double count_omega(int l, int k, double q = .6e-1) const;	// for counting Gaussian function
 
 	bool operator < (const Solution & other) { return F < other.F; }	// for sort
